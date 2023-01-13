ad8fc2c8c88b21ff3ea557641db489d000e71733
VanAn <nguyenvanan28730@gmail.com>
2023-01-13 13:48:48 +0900

    change-color-of-sudoku-result

--- sudoku_project/sudoku_app/views.py
+++ sudoku_project/sudoku_app/views.py
@@ -26,12 +26,34 @@ def sudoku_image_input_view(request):
                     element = str(row)+str(col)
                     if request.POST.get(element) != "":
                         matrix_input[row][col] = request.POST.get(element)
-            print(f'Maxtrix input', matrix_input)
+            
             matrix_result=SudokuAlgorithm.soduku_algorithm(matrix_input)
             matrix_result=matrix_result.tolist()
-            reg_matrix = matrix_result
-            #return render(request,'sudoku_app/display_sloved_result.html', {'matrix_input':matrix_input},{'matrix_result': matrix_result})
-            return render(request,'sudoku_app/display_sudoku_result2.html', {'reg_matrix': reg_matrix})
+            matrix_input=matrix_input.tolist()
+
+            matrix_input1 = [[5, 7, 0, 3, 6, 4, 2, 9, 8], 
+                            [6, 3, 0, 7, 9, 2, 4, 1, 5], 
+                            [9, 4, 2, 0, 1, 5, 3, 0, 7], 
+                            [3, 2, 4, 0, 5, 8, 6, 7, 9], 
+                            [8, 6, 9, 2, 0, 7, 5, 0, 1], 
+                            [7, 1, 5, 6, 0, 9, 8, 3, 2], 
+                            [1, 8, 6, 9, 2, 0, 7, 5, 4], 
+                            [2, 5, 3, 4, 7, 0, 9, 8, 6], 
+                            [4, 9, 7, 5, 8, 6, 1, 0, 3]]
+
+            matrix_result1 = [[5, 3, 7, 1, 6, 4, 2, 9, 8], 
+                            [6, 3, 4, 7, 9, 2, 4, 1, 5], 
+                            [9, 4, 2, 7, 1, 5, 3, 0, 7], 
+                            [3, 2, 4, 9, 5, 8, 6, 7, 9], 
+                            [8, 6, 9, 2, 4, 7, 5, 0, 1], 
+                            [7, 1, 5, 6, 2, 9, 8, 3, 2], 
+                            [1, 8, 6, 9, 2, 8, 7, 5, 4], 
+                            [2, 5, 3, 4, 7, 9, 9, 8, 6], 
+                            [4, 9, 7, 5, 8, 6, 1, 2, 3]]
+
+            print(f'Maxtrix input1', matrix_input1)
+            print(f'Maxtrix result', matrix_result)
+            return render(request,'sudoku_app/display_sloved_result.html', {'matrix_result1': matrix_result1, 'matrix_input1': matrix_input1})
 
         else: print("Do not recognize the POST")
     else:
